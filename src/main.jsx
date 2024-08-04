ad16b63d16a24263c0dc4bcb5ff8299d8fc55c06
Imran Tan <imrantan94@gmail.com>
2024-08-04 23:22:50 +0800

    Updated the way the background images load.

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,13 @@ import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import bamboo from './assets/images/backgrounds/bamboo.jpg';
+import seamlessPattern from './assets/images/backgrounds/seamless-geometric-pattern.jpg';
+import l986 from './assets/images/backgrounds/l986.jpg';
+import freePattern from './assets/images/backgrounds/free_pattern_1.png'; 
 
 const setRandomBackgroundImage = () => {
-  const images = [
-    'src/assets/images/backgrounds/bamboo.jpg',
-    'src/assets/images/backgrounds/seamless-geometric-pattern.jpg',
-    'src/assets/images/backgrounds/l986.jpg',
-    'src/assets/images/backgrounds/free_pattern_1.png'
-  ];
-
+  const images = [bamboo, seamlessPattern, l986, freePattern];
   const randomImage = images[Math.floor(Math.random() * images.length)];
   document.body.style.setProperty('--background-image', `url(${randomImage})`);
 };
